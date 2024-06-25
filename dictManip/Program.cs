2e6ae96e6b0f4fc25895e6589458a157999cc954
CaePolit <caepolit@gmail.com>
2024-06-25 11:24:27 +0200

    Ajoutez des fichiers projet.

---
+++ dictManip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Console.Write("premier chaine: ");
+        Dictionary<char, string> myDictionary = new Dictionary<char, string>
+        {
+        { 'a' , "apple"},
+        { 'b' , "bee"},
+        { 'c' , "carrot" }
+        };
+        //Console.WriteLine(myDictionary);
+        myDictionary.Remove('c');
+        //Console.WriteLine(myDictionary);
+
+    }
+}
